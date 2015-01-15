e9b35909b780cc794353bd5dabfbaf46ea4a8c51
Markus Mayer <development@widemeadows.de>
2015-01-15 19:45:39 +0100

    initial commit

---
+++ GameWindow/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GameWindow
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renderTarget1 = new GameWindow.RenderTarget();
+            this.SuspendLayout();
+            // 
+            // renderTarget1
+            // 
+            this.renderTarget1.Location = new System.Drawing.Point(12, 12);
+            this.renderTarget1.Name = "renderTarget1";
+            this.renderTarget1.Size = new System.Drawing.Size(641, 481);
+            this.renderTarget1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 505);
+            this.Controls.Add(this.renderTarget1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Snake\'s on a Pane";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RenderTarget renderTarget1;
+    }
+}
+
---
+++ GameWindow/RenderTarget.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameWindow
+{
+    /// <summary>
+    /// Class RenderTarget.
+    /// </summary>
+    public partial class RenderTarget : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RenderTarget"/> class.
+        /// </summary>
+        public RenderTarget()
+        {
+            InitializeComponent();
+
+            const ControlStyles styles = ControlStyles.Opaque
+                                         | ControlStyles.OptimizedDoubleBuffer
+                                         | ControlStyles.AllPaintingInWmPaint
+                                         | ControlStyles.DoubleBuffer
+                                         | ControlStyles.UserPaint;
+            SetStyle(styles, true);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Forms.Control.Paint" /> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.Windows.Forms.PaintEventArgs" /> that contains the event data.</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // base.OnPaint(e);
+
+            var gr = e.Graphics;
+            
+            const int gridWidth = 10;
+            
+            var left = ClientRectangle.Left;
+            var top = ClientRectangle.Top;
+            var width = ClientRectangle.Width;
+            var height = ClientRectangle.Height;
+
+            var pen = new Pen(Color.FromArgb(10, 10, 10));
+
+            gr.Clear(Color.Black);
+
+            for (var y = top; y <= height; y += gridWidth)
+            {
+                gr.DrawLine(pen, 0, y, width, y);
+            }
+
+            for (var x = left; x <= width; x += gridWidth)
+            {
+                gr.DrawLine(pen, x, 0, x, height);
+            }
+        }
+    }
+}
