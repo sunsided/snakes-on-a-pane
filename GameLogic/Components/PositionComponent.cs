f105b65ac16af2e2bc5ea548d4961b1bc27011eb
Markus Mayer <development@widemeadows.de>
2015-01-15 20:45:29 +0100

    started with a position component

---
+++ GameLogic/Components/PositionComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace GameLogic.Components
+{
+    /// <summary>
+    /// Struct PositionComponent
+    /// <para>
+    ///     Describes the position of an entity.
+    /// </para>
+    /// </summary>
+    struct PositionComponent : IComponent
+    {
+        /// <summary>
+        /// The x position
+        /// </summary>
+        public int X;
+
+        /// <summary>
+        /// The y position
+        /// </summary>
+        public int Y;
+    }
+}
