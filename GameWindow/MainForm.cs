02bb2d591bd555cd2430f67bf569d554a808021e
Markus Mayer <development@widemeadows.de>
2015-01-16 23:59:43 +0100

    added buffer factory events

--- GameWindow/MainForm.cs
+++ GameWindow/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using GameWindow.Rendering;
 
 namespace GameWindow
 {
@@ -8,6 +10,11 @@ namespace GameWindow
     public partial class MainForm : Form
     {
         /// <summary>
+        /// Occurs when the buffer factory is ready.
+        /// </summary>
+        public event EventHandler<BufferFactoryEventArgs> BufferFactoryReady;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="MainForm"/> class.
         /// </summary>
         public MainForm()
@@ -16,11 +23,31 @@ namespace GameWindow
         }
 
         /// <summary>
+        /// Raises the <see cref="E:System.Windows.Forms.Form.Shown" /> event.
+        /// </summary>
+        /// <param name="e">A <see cref="T:System.EventArgs" /> that contains the event data.</param>
+        protected override void OnShown(EventArgs e)
+        {
+            OnBufferFactoryReady(new BufferFactoryEventArgs(renderTarget1));
+            base.OnShown(e);
+        }
+
+        /// <summary>
         /// Renders the frame.
         /// </summary>
         public void RenderFrame()
         {
             renderTarget1.Render();
+        }
+
+        /// <summary>
+        /// Handles the <see cref="E:BufferFactoryReady" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="BufferFactoryEventArgs"/> instance containing the event data.</param>
+        protected virtual void OnBufferFactoryReady(BufferFactoryEventArgs e)
+        {
+            var handler = BufferFactoryReady;
+            if (handler != null) handler(this, e);
         }
     }
 }
---
+++ GameWindow/Rendering/BufferFactoryEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace GameWindow.Rendering
+{
+    /// <summary>
+    /// Class BufferFactoryEventArgs.
+    /// </summary>
+    public class BufferFactoryEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the factory.
+        /// </summary>
+        /// <value>The factory.</value>
+        [NotNull]
+        public IBufferFactory Factory { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BufferFactoryEventArgs" /> class.
+        /// </summary>
+        /// <param name="factory">The factory.</param>
+        /// <exception cref="System.ArgumentNullException">The factory must not be null</exception>
+        public BufferFactoryEventArgs([NotNull] IBufferFactory factory)
+        {
+            if (ReferenceEquals(factory, null)) throw new ArgumentNullException("factory", "The factory must not be null");
+
+            Factory = factory;
+        }
+    }
+}
