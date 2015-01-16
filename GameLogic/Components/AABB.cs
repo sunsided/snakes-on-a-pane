a2d7ba32871aae3d11b0bdc6a48c114d9aec7ed0
Markus Mayer <development@widemeadows.de>
2015-01-16 02:49:17 +0100

    added an AABB component

---
+++ GameLogic/Components/AABB.cs
@@ -0,0 +1,21 @@
+﻿namespace GameLogic.Components
+{
+    /// <summary>
+    /// Struct AABB
+    /// <para>
+    ///     Axis-aligned bounding box of the entity
+    /// </para>
+    /// </summary>
+    struct AABB : IComponent
+    {
+        /// <summary>
+        /// The box width
+        /// </summary>
+        public float Width;
+
+        /// <summary>
+        /// The box height
+        /// </summary>
+        public float Height;
+    }
+}
