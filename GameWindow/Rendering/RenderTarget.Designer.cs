0e5c2378b3a963f58889b3878554975bd0e7890b
Markus Mayer <development@widemeadows.de>
2015-01-17 00:41:07 +0100

    fixed dimensions to 640x480

--- GameWindow/Rendering/RenderTarget.Designer.cs
+++ GameWindow/Rendering/RenderTarget.Designer.cs
@@ -37,6 +37,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.MaximumSize = new System.Drawing.Size(640, 480);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
             this.Name = "RenderTarget";
             this.Size = new System.Drawing.Size(640, 480);
             this.ResumeLayout(false);
