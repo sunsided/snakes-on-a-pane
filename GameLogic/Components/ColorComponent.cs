52a88288f0afbb2e10557c263525d81dd8e87227
Markus Mayer <development@widemeadows.de>
2015-01-16 02:49:58 +0100

    added a color component

---
+++ GameLogic/Components/ColorComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace GameLogic.Components
+{
+    /// <summary>
+    /// Struct ColorComponent
+    /// <para>
+    ///     The color
+    /// </para>
+    /// </summary>
+    struct ColorComponent : IComponent
+    {
+        /// <summary>
+        /// The color
+        /// </summary>
+        public Color Color;
+    }
+}
