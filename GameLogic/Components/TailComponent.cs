8b216ddc76a303bd08cf4e9c5b60fe9fa36e8695
Markus Mayer <development@widemeadows.de>
2015-01-17 19:47:00 +0100

    now using a linked list for tail crease storage

--- GameLogic/Components/TailComponent.cs
+++ GameLogic/Components/TailComponent.cs
@@ -11,13 +11,44 @@ namespace GameLogic.Components
     public sealed class TailComponent : IComponent
     {
         /// <summary>
+        /// Struct Crease
+        /// </summary>
+        public sealed class Crease
+        {
+            /// <summary>
+            /// The x position
+            /// </summary>
+            public float X;
+            
+            /// <summary>
+            /// The y position
+            /// </summary>
+            public float Y;
+
+            /// <summary>
+            /// The x velocity after the crease
+            /// </summary>
+            public float NextXVelocity;
+
+            /// <summary>
+            /// The y velocity after the crease
+            /// </summary>
+            public float NextYVelocity;
+
+            /// <summary>
+            /// The distance from the head
+            /// </summary>
+            public float DistanceFromHead;
+        }
+
+        /// <summary>
         /// The snake's tail length
         /// </summary>
-        public int Length;
+        public float Length;
 
         /// <summary>
         /// The creases in the tail
         /// </summary>
-        public readonly Stack<PositionComponent> Creases = new Stack<PositionComponent>();
+        public readonly LinkedList<Crease> Creases = new LinkedList<Crease>();
     }
 }
--- GameLogic/Systems/TailCreaseSystem.cs
+++ GameLogic/Systems/TailCreaseSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using GameLogic.Components;
 
 namespace GameLogic.Systems
@@ -21,10 +22,7 @@ namespace GameLogic.Systems
             DirectionChangeComponent directionChange;
             if (!entity.TryGetComponent(out directionChange)) return;
             Debug.Assert(directionChange != null, "directionChange != null");
-
-            // early exit
-            if (!directionChange.DirectionChanged) return;
-
+            
             // fetch the tail component
             TailComponent tail;
             if (!entity.TryGetComponent(out tail)) return;
@@ -35,8 +33,60 @@ namespace GameLogic.Systems
             if (!entity.TryGetComponent(out position)) return;
             Debug.Assert(position != null, "position != null");
 
-            // the direction has changed, so add a crease at the current position
-            tail.Creases.Push(new PositionComponent {X = position.X, Y = position.Y});
+            // fetch the velocity component
+            VelocityComponent vc;
+            if (!entity.TryGetComponent(out vc)) return;
+            Debug.Assert(vc != null, "vc != null");
+            
+            // as we move forward, increment the distance of each crease
+            var velocity = Math.Abs(vc.X) + Math.Abs(vc.Y);
+            var pointer = tail.Creases.First;
+            while (pointer != null)
+            {
+                var distance = (pointer.Value.DistanceFromHead += velocity);
+
+                // if this segment is now at a position after the tail's end,
+                // move it one step towards it's velocity direction
+                if (distance > tail.Length)
+                {
+                    pointer.Value.X += pointer.Value.NextXVelocity;
+                    pointer.Value.Y += pointer.Value.NextYVelocity;
+                    pointer.Value.DistanceFromHead -= velocity;
+
+                    // since we moved the tail's end, we may now be
+                    // at the same position as the previous crease
+                    // (if it exists), so check for that and delete if necessary
+                    var prev = pointer.Previous;
+                    const float epsilon = 0.0001F;
+                    if (prev != null 
+                        && Math.Abs(prev.Value.X - pointer.Value.X) < epsilon 
+                        && Math.Abs(prev.Value.Y - pointer.Value.Y) < epsilon)
+                    {
+                        tail.Creases.Remove(pointer);
+
+                        // point to the previous item, so that the finishing
+                        // traversal to "pointer.Next" yields the correct result
+                        pointer = prev;
+                    }
+                }
+
+                // take care of the next segment
+                pointer = pointer.Next;
+            }
+
+            // early exit
+            if (directionChange.DirectionChanged)
+            {
+                // the direction has changed, so add a crease at the current position
+                tail.Creases.AddFirst(new TailComponent.Crease
+                                      {
+                                          X = position.X,
+                                          Y = position.Y,
+                                          DistanceFromHead = 0,
+                                          NextXVelocity = vc.X,
+                                          NextYVelocity = vc.Y
+                                      });
+            }
         }
     }
 }
