5a830cfad08bdbd326376fa385d2cdf554b97ed2
Markus Mayer <development@widemeadows.de>
2015-01-17 02:33:05 +0100

    added SystemBase class

---
+++ GameLogic/SystemBase.cs
@@ -0,0 +1,14 @@
+﻿namespace GameLogic
+{
+    /// <summary>
+    /// Class SystemBase.
+    /// </summary>
+    public abstract class SystemBase : ISystem
+    {
+        /// <summary>
+        /// Processes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        public abstract void Process(IEntity entity);
+    }
+}
