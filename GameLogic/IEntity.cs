838b509abeff9c17569a054ab7514f8ba21bbff5
Markus Mayer <development@widemeadows.de>
2015-01-17 01:41:57 +0100

    added TryGetComponent to IEntity

--- GameLogic/IEntity.cs
+++ GameLogic/IEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace GameLogic
+﻿using JetBrains.Annotations;
+
+namespace GameLogic
 {
     /// <summary>
     /// Interface IEntity
@@ -8,5 +10,13 @@
     /// </summary>
     public interface IEntity
     {
+        /// <summary>
+        /// Tries to obtains the component of the given type.
+        /// </summary>
+        /// <typeparam name="TComponent">The type of the component.</typeparam>
+        /// <param name="component">The component, if it exists.</param>
+        /// <returns><see langword="true" /> if the component exists, <see langword="false" /> otherwise.</returns>
+        bool TryGetComponent<TComponent>([CanBeNull] out TComponent component)
+            where TComponent : IComponent;
     }
 }
